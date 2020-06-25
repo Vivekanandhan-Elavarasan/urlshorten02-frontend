bf19f01fbeeb85932b48aa0bb5c4dbbc9c3c2bed
Vivekanandhan-Elavarasan <63054867+Vivekanandhan-Elavarasan@users.noreply.github.com>
2020-06-25 15:41:30 +0530

    change in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiURL : "https://urlshorten-02.herokuapp.com"
+  production: false,
 };
 
 /*
